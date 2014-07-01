460b63c6d73945de2a236f833ae2db15004d79ba
hoho <kalle.last@gmail.com>
2014-07-02 01:18:46 +0300

    ZTech - altering research difficulty of Factorio
    This mod tries to make research more interesting in Factorio. At the
    moment it scans through all the researches, including stuff added by
    mods, and makes the recipes that take >1 science packs to use more of
    the lower-tier packs.

---
+++ data.lua
@@ -0,0 +1,37 @@
+function modIngredients(ingredients)
+    local multipliers = { 1, 2, 4, 8 }
+    local total = #ingredients
+    for i = 1, total do
+        local pack = ingredients[i][1]
+	local count = ingredients[i][2]
+	if (pack == "science-pack-1")
+	then
+	    ingredients[i][2] = count*multipliers[total]
+	elseif (pack == "science-pack-2")
+	then
+	    if ((total-1) < 1) then
+		ingredients[i][2] = 1000
+	    else
+		ingredients[i][2] = count*multipliers[total-1]
+	    end
+	elseif (pack == "science-pack-3")
+	then
+	    if ((total-2) < 1) then
+		ingredients[i][2] = 1000
+	    else
+		ingredients[i][2] = count*multipliers[total-2]
+	    end
+	elseif (pack == "alien-science-pack")
+	then
+	    if ((total-3) < 1) then
+		ingredients[i][2] = 1000
+	    else
+		ingredients[i][2] = count*multipliers[total-3]
+	    end
+	end
+    end
+end
+
+for k,v in pairs(data.raw.technology) do
+	modIngredients(v.unit.ingredients)
+end
